d3a920b03c9d87141b224f5aa66b05a447007764
Uladzimir_Paliakou <to.polyakov.vova@gmail.com>
2017-10-18 18:08:03 +0300

    Third task mistakes

--- utils/streams.js
+++ utils/streams.js
@@ -1,26 +1,57 @@
 import program from 'commander';
 import reducer from './reducer/reducer'
+import {actions} from "./reducer/actions.json"
+import * as fileWorker from "./reducer/controller/fileStreamController"
 
-export const comandLine = ()=> program
-    .version('0.1.0')
-    .option('-a, --action', 'choose and action')
-    .option('-f, --file', 'file path')
-    .action(reducer)
-    .parse(process.argv);
 
-program.on('--help', () =>
+const helpMsg = () =>
   console.log(`
+        commands Example
     $ ./streams --action=io --file=MOCK_DATA.csv
     $ ./streams --action=transform-file --file=MOCK_DATA.csv
     $ ./streams -a io -f MOCK_DATA.csv
     $ ./streams --help
     $ ./streams -h
-             `));
+             `);
+export const commandLine = () => {
+  program
+    .version('0.1.0')
+    .option('-a, --action <action> ', 'choose and action')
+    .option('-f, --file <file> ', 'file path')
+    .parse(process.argv);
 
+  program.on('--help',helpMsg);
 
-if (!module.parent) {
-  comandLine()
+  const actionItem = program.action;
+  const fileItem = program.file;
+
+  if (!actionItem || !fileItem) {
+    helpMsg()
+  }
+  else {
+
+    switch (actionItem.toLowerCase()) {
+      case actions.IO:
+        fileWorker.fileReader(fileItem);
+        break;
+      case actions.TRANSFROM_FILE:
+        fileWorker.fileUpdater(fileItem);
+        break;
+      case actions.CONVERT :
+        fileWorker.fileTransformer(fileItem);
+        break;
+      case actions.IOJSON:
+        fileWorker.cvsTojsonIO(fileItem);
+        break;
+      default :
+        console.error('no action has been given!!!!');
+        process.exit(1);
+        break;
+    }
+
+  }
 }
 
-
-
+if(!module.parent){
+  commandLine()
+}
