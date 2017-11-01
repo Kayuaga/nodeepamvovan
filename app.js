7c938e1f437b5981e6c4700b13f6a7a04478cfa9
Kayuga <Uladzimir_Paliakou@epam.com>
2017-11-01 23:56:53 +0300

    fourth task

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cookies  from './middlewares/cookieParser';
+import queryParser from './middlewares/queryParser';
+import bodyParser from 'body-parser';
+const productMap = new Map();
+let id =1 ;
+const product = {
+    name: 'Supreme T-Shirt', brand: 'Supreme',
+    price: 99.99,
+    options: [
+        { color: 'blue' },
+        { size: 'XL' } ]
+}
+
+productMap.set(id,product);
+
 const app = express();
 app.use(cookieParser());
-app.use((req,res,next)=> {
+app.use(cookies);
+app.use(queryParser);
+app.use(bodyParser.json());
+app.get('/',(req,res)=>{
+    console.log(req.parsedCoockie)
+    console.log(req.parsedQuery);
 })
+app.get('/api/products', (req, res) => {
+   res.send(productMap)
+});
+
+app.get('/api/products/:id',(req,res) =>{
+    console.log(req.params.id);
+    res.send(productMap.get(Number(req.params.id))||{});
+});
+
+app.post('/api/products',(req,res)=>{
+    id+=1;
+    productMap.set(id,(req.body));
+    res.send(productMap);
+
+});
 export default  app;
