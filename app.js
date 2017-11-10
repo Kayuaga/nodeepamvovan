8d0caed35db70c9a2ddf6cfcb5bf823bba9b0212
Kayuaga <to.polyakov.vova@gmail.com>
2017-11-10 17:22:51 +0300

    auth added

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cookies  from './middlewares/cookieParser';
 import queryParser from './middlewares/queryParser';
+import authMiddle from './middlewares/authMiddle';
+import passportLocal from './middlewares/localStratagy';
 import bodyParser from 'body-parser';
-import productRoute from './routes/productRoutes'
-import userRoute from './routes/userRoute'
+import productRoute from './routes/productRoutes';
+import userRoute from './routes/userRoute';
+import authRoute from './routes/authRoutes';
+
+
 const productMap = new Map();
 let id =1 ;
 const product = {
@@ -27,7 +32,10 @@ app.get('/',(req,res)=>{
     console.log(req.parsedCoockie)
     console.log(req.parsedQuery);
 })
-
+//app.use('/api',middle)
+//app.use(authRoute)
+//app.use('/api',authMiddle);
+app.use('/auth',passportLocal)
 app.use('/api/products',productRoute);
 app.use('/api/users',userRoute);
 
